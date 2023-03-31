ad97277b7aa7011a621c2171eba3e456a5253189
lawkelvin33 <lawkelvin33@gmail.com>
2023-03-31 13:03:26 +0900

    12345

--- src/api/birth/birth.service.ts
+++ src/api/birth/birth.service.ts
@@ -1,17 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Birth } from '@prisma/client';
 import { BirthHistoryInfo } from './interface/birth-info.interface';
 import { BirthHistoryDto } from './dto/birth-history.dto';
 import { RegisterBirthTxPayload } from './payload/register-birth-tx.payload';
-import {ethers} from 'ethers';
+import { ethers } from 'ethers';
+import { EventExtractService } from './event-extract.service';
 
 @Injectable()
 export class BirthService {
-    const provider = new ethers.providers.JsonRpcProvider("https://public-node-api.klaytnapi.com/v1/baobab");
-  
-    async addBirthData(
-    payload: RegisterBirthTxPayload,
-  ){
+  constructor(private readonly eventExtractService: EventExtractService) {}
+  provider = new ethers.providers.JsonRpcProvider(
+    'https://public-node-api.klaytnapi.com/v1/baobab',
+  );
 
-  } Promise<BirthHistoryDto> {}
+  async addBirthData(payload: RegisterBirthTxPayload) {
+    const birthEvents = this.eventExtractService.extractBirthEvent(
+      this.provider,
+      payload.txHash,
+      payload.address,
+    );
+    if (birthEvents.length === 0) {
+      throw new BadRequestException('No birth events in this hash.');
+    }
+  }
 }
--- src/api/birth/event-extract.service.ts
+++ src/api/birth/event-extract.service.ts
@@ -4,10 +4,14 @@ import { providers } from 'ethers';
 @Injectable()
 export class EventExtractService {
   constructor() {}
+  //TODO : BIRTH_SIGNATURE 값 수정해줘야함
+  private readonly BIRTH_SIGNATURE =
+    '0x4c209b5fc8ad50758f13e2e1088ba56a560dff690a1c6fef26394f4c03821c4f';
   private async validateTxHash(
     provider: providers.Provider,
     txHash: string,
   ): Promise<providers.TransactionReceipt> {
+    //이걸로 transaction block 전체를 받아옴... 원하는 정보는 receipt.log에 있음!
     const receipt = await provider.getTransactionReceipt(txHash);
     if (!receipt)
       throw new BadRequestException(
@@ -21,7 +25,17 @@ export class EventExtractService {
     txHash: string,
     address: string,
   ) {
+    //provider로부터 txHash를 통해서 필요한 block를 받아옴
     const receipt = await this.validateTxHash(provider, txHash);
-    //TODO
+    //이거 왜 필요함?? receipt를 받아오는 시간?
+    const createdAt = await provider
+      .getBlock(receipt.blockNumber)
+      .then((block) => new Date(block.timestamp * 1000));
+    //받아온 block에서 필요한 정보만 추출 (address, BIRTH_SIGNATURE로 확인)
+    const birthEventLogs = receipt.logs.filter(
+      (log) =>
+        log.address === address && log.topics[0] === this.BIRTH_SIGNATURE,
+    );
+    //추출한 정보에서 다시 한번 filtering이 필요함
   }
 }
