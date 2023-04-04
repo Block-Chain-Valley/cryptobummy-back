8c087d636390a7ea5eabf19a23f38d340f6ca284
lawkelvin33 <lawkelvin33@gmail.com>
2023-04-04 13:44:10 +0900

    ㅠㅠ

--- src/api/exhaust/exhaust.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ExhaustController } from './exhaust.controller';
-import { ExhaustService } from './exhaust.service';
-
-@Module({
-  controllers: [ExhaustController],
-  providers: [ExhaustService]
-})
-export class ExhaustModule {}
